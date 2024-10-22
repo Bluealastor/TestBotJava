cc526575d6c93ad0d28b6fbf90bb926343cec51b
Christian <chrise79456@gmail.com>
2024-10-23 01:35:42 +0200

    first commit

---
+++ src/main/java/it/BlueAlastor/Main.java
@@ -0,0 +1,11 @@
+package it.BlueAlastor;
+
+import it.BlueAlastor.service.MovementMouse;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        MovementMouse mouse = new MovementMouse();
+
+       mouse.mouseMovement();
+    }
+}
---
+++ src/main/java/it/BlueAlastor/service/MovementMouse.java
@@ -0,0 +1,35 @@
+package it.BlueAlastor.service;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+
+public class MovementMouse {
+
+    public void mouseMovement() throws InterruptedException {
+        int xCord = 200;
+        int yCord = 200;
+        while(true){
+            try {
+                Robot robot = new Robot();
+                robot.mouseMove(xCord, yCord);
+                xCord += 1;
+                yCord += 1;
+                robot.mouseMove(xCord, yCord);
+                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                Thread.sleep(4000);
+                xCord -= 1;
+                yCord -= 1;
+                robot.mouseMove(xCord, yCord);
+                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                Thread.sleep(4000);
+            }catch ( AWTException e){
+                System.out.println(e);
+
+            }
+        }
+
+    }
+
+}
